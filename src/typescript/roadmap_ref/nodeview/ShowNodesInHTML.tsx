411aab07f6f249405ba57751c00b659fb0c9dd3c
antonel-iwnl <antonioadelin@gmail.com>
2023-07-21 19:24:36 +0300

    [Fri 21/07/2023] (Fix) Nodes Rendering

--- src/typescript/roadmap_ref/nodeview/ShowNodesInHTML.tsx
+++ src/typescript/roadmap_ref/nodeview/ShowNodesInHTML.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import { getNodeById } from '@store/roadmap/data/roadmap_static';
+import { get } from 'astro/dist/assets/image-endpoint';
+import { NodeClass } from '@typescript/roadmap_ref/node/core/core';
+import { factoryComponentJSONEmpty } from '@typescript/roadmap_ref/node/components/text/factories';
+import { ComponentTitle } from '@typescript/roadmap_ref/node/components/text/core';
 import NodeView from './NodeView';
-import nodesData from './nodesplaceholders.json';
 
 const ShowNodesInHTML = () => {
+  const node = new NodeClass();
+  node.data.id = 'node1';
+  node.properties.width = 500;
+  node.properties.height = 500;
+  node.componentsJSON[0] = factoryComponentJSONEmpty('Title');
+  node.componentsJSON[1] = factoryComponentJSONEmpty('Description');
+  node.nestedNodesIds = [
+    'node2',
+    'node3',
+    'node4',
+    'node5',
+    'node6',
+    'node7',
+    'node8',
+    'node9',
+  ];
   return (
     <div className='w-screen h-screen bg-[#ECEFF2] flex'>
-      <NodeView node={nodesData.node} />
+      <NodeView node={node} />
     </div>
   );
 };
