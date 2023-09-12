c8bc6e56e1662ff658f2700ad33db182e247bd36
sopy <contact@sopy.one>
2023-09-12 18:28:09 +0300

    Add fallback for base64 encoding/decoding

--- src/typescript/utils/misc.ts
+++ src/typescript/utils/misc.ts
@@ -9,9 +9,13 @@ export function getRandomId() {
 }
 
 export function decodeBase64(str: string) {
+  if (!Buffer) return atob(str);
+
   return Buffer.from(str, 'base64').toString();
 }
 
 export function encodeBase64(str: string) {
+  if (!Buffer) return btoa(str);
+
   return Buffer.from(str).toString('base64');
 }
