8c490f8114330c8ecfcd8c3421e3e829187ed246
sopy <contact@sopy.one>
2023-09-15 20:52:41 +0300

    Add new safe domains to urlUtils

--- src/typescript/utils/urlUtils.ts
+++ src/typescript/utils/urlUtils.ts
@@ -3,13 +3,78 @@ import { parseDomain, ParseResultType } from 'parse-domain';
 
 const safeUrlDomains = [
   'cs.fyi',
+  'freecodecamp.org',
   'github.com',
+  'google.com',
   'mozilla.org',
   'navigolearn.com',
   'roadmap.sh',
   'wikipedia.org',
   'youtu.be',
   'youtube.com',
+
+  // learn x online org domains
+  'learnpython.org',
+  'learnjavaonline.org',
+  'learn-html.org',
+  'learn-golang.org',
+  'learn-c.org',
+  'learn-cpp.org',
+  'learn-js.org',
+  'learn-php.org',
+  'learnshell.org',
+  'learncs.org',
+  'learn-perl.org',
+  'learnrubyonline.org',
+  'learnscala.org',
+  'learnsqlonline.org',
+
+  // javascript specific
+  'javascript.com',
+  'javascript.info',
+  'learnjavascript.online',
+  'javascript30.com',
+  'reaal.me',
+
+  // react specific
+  'reactjs.org',
+  'react.dev',
+  'egghead.io',
+  'react-tutorial.app',
+
+  // PHP specific
+  'php.net',
+  'phptherightway.com',
+  'phpdelusions.net',
+  'phpapprentice.com',
+
+  // SQL specific
+  'sqlbolt.com',
+  'sqlzoo.net',
+  'sqltutorial.org',
+  'sqlteaching.com',
+  'sqlfiddle.com',
+  'sql-ex.com',
+
+  // Python specific
+  'python.org',
+  'pythonspot.com',
+  'pythonprogramming.net',
+  'pythonforbeginners.com',
+  'python-course.eu',
+  'pythoncentral.io',
+  'pythoncheatsheet.org',
+  'pythonforbiologists.com',
+  'pythonforundergradengineers.com',
+  'pythonforfinance.net',
+
+  // games
+  'flexboxfroggy.com',
+  'codeabbey.com',
+  'codingame.com',
+  'hackerrank.com',
+  'codewars.com',
+  'exercism.org',
 ];
 
 export function getDomainFromUrl(url: string) {
