434a229cfb15a5cfaa6e50bf264fe50c74d609d8
sopy <contact@sopy.one>
2023-09-17 23:50:56 +0300

    Got rid of Shadow in the right place

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -189,7 +189,7 @@ const Home = () => {
                 ry='4'
                 width='6rem'
                 height='2rem'
-                className='fill-white stroke-slate-200 border-[1px] flex rounded-lg justify-center drop-shadow-md items-center'
+                className='fill-white stroke-slate-200 border-[1px] flex rounded-lg justify-center items-center'
               />
             );
           })}
@@ -226,7 +226,7 @@ const Home = () => {
           <motion.a
             type='button'
             href='/explore'
-            className='mx-auto mt-8 px-5 py-2 text-white bg-primary rounded-lg text-xl font-roboto-text font-medium'
+            className='mx-auto mt-8 px-5 py-2 text-white bg-primary rounded-lg shadow-md text-xl font-roboto-text font-medium'
             whileHover={{
               backgroundColor: '#1A1B50',
               color: '#fff',
