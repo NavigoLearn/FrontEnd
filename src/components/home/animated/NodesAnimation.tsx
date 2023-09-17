f6c3fd720599171810c01bd143d0eafda93f04a2
sopy <contact@sopy.one>
2023-09-17 18:27:25 +0300

    Improve NodesAnimation and MiddleSection components
    This commit enhances the NodesAnimation component by adding rotation angles and gradient lines to animations. Removal of unused import ('framer-motion') has also been done. In MiddleSection, HTML classes were adjusted to fix element positioning and layering.
    
    Co-authored-by: antonel-iwnl <antonioadelin@gmail.com>

--- src/components/home/animated/NodesAnimation.tsx
+++ src/components/home/animated/NodesAnimation.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { motion } from 'framer-motion';
 
 type INodesAnimation = {
   x1?: number;
@@ -30,25 +29,87 @@ const NodesAnimation = ({
   height,
   strokeOpacity,
 }: INodesAnimation) => {
+  let swappedX = false;
+  let swappedY = false;
+
+  const angle = (Math.atan2(y2 - y1, x2 - x1) * 180) / Math.PI;
+  const isStraight = x1 === x2 || y1 === y2;
+
+  // calculate gradients based on the angle
+  const angleInRadians = (angle * Math.PI) / 180;
+  const gradX1 = 0.5 + Math.cos(angleInRadians) * 0.5;
+  const gradY1 = 0.5 + Math.sin(angleInRadians) * 0.5;
+  const gradX2 = 0.5 - Math.cos(angleInRadians) * 0.5;
+  const gradY2 = 0.5 - Math.sin(angleInRadians) * 0.5;
+
+  if (x2 < x1) {
+    // if x2 is less than x1, swap them
+    [x1, x2] = [x2, x1];
+    swappedX = true;
+  }
+  if (y2 < y1) {
+    // if y2 is less than y1, swap them
+    [y1, y2] = [y2, y1];
+    swappedY = true;
+  }
+
   return (
     <svg
       width={width}
       height={height}
-      viewBox='-4 -79 79 4'
+      viewBox={`${x1 - 3} ${y1 - 3} ${x2 + 3} ${y2 + 3}`}
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
     >
-      <motion.line
-        x1={x1}
-        x2={x2}
-        y1={y1}
-        y2={y2}
+      <defs>
+        MiddleSection
+        <linearGradient
+          id={`gradientLine${angle}`}
+          x1={`${(isStraight ? gradX2 : gradX1) * 100}%`}
+          y1={`${(isStraight ? gradY2 : gradY1) * 100}%`}
+          x2={`${(isStraight ? gradX1 : gradX2) * 100}%`}
+          y2={`${(isStraight ? gradY1 : gradY2) * 100}%`}
+        >
+          <stop offset='0%' stopColor='white' stopOpacity={1} />
+          <stop
+            offset={isStraight ? '40%' : '60%'}
+            stopColor={isStraight ? 'black' : 'white'}
+            stopOpacity={1}
+          />
+          <stop offset='100%' stopColor='black' stopOpacity={0} />
+        </linearGradient>
+        <mask id={`maskLine${angle}`}>
+          <rect
+            x={x1 - 3}
+            y={y1 - 3}
+            width={Math.abs(x2 - x1) + 6}
+            height={Math.abs(y2 - y1) + 6}
+            fill={`url(#gradientLine${angle})`}
+          />
+        </mask>
+      </defs>
+      {/* debugging mask */}
+      {/* <rect */}
+      {/*  mask={`url(#maskLine${angle})`} */}
+      {/*  fill='black' */}
+      {/*  x={x1} */}
+      {/*  y={y1} */}
+      {/*  width={x2 - x1} */}
+      {/*  height={y2 - y1} */}
+      {/* /> */}
+
+      <line
+        x1={swappedX ? x2 : x1}
+        x2={swappedX ? x1 : x2}
+        y1={swappedY ? y2 : y1}
+        y2={swappedY ? y1 : y2}
         d='M4 2H75'
         stroke='#3361D8'
         strokeOpacity={strokeOpacity}
-        strokeWidth='2'
-        strokeDasharray='2 6'
+        strokeWidth='3'
+        strokeDasharray='4 12'
         strokeLinecap='round'
+        mask={!isStraight ? `url(#maskLine${angle})` : undefined}
       >
         <animate
           attributeName='stroke-dashoffset'
@@ -57,7 +118,7 @@ const NodesAnimation = ({
           dur='0.3s'
           repeatCount='indefinite'
         />
-      </motion.line>
+      </line>
     </svg>
   );
 };
