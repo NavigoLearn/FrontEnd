bec8e649d023c26c15b7e0546dcacc7e9aab6ee7
sopy <contact@sopy.one>
2023-09-18 22:46:14 +0300

    Refactor: Arrange stuff into folders and remove old files
    Co-authored-by: antonel-iwnl <antonioadelin@gmail.com>

--- src/components/home/MiddleSection.tsx
+++ src/components/home/MiddleSection.tsx
@@ -110,73 +110,27 @@ const MiddleSection = () => {
       </motion.div>
       <div className='w-full mt-56 flex flex-row justify-center items-center'>
         <div className='flex flex-row gap-32'>
-          <motion.div
-            className='border-primary border-t-4 w-72 h-80 2xl:w-96 py-3 px-6 bg-white drop-shadow-md items-center'
-            initial='hidden'
+          <Cards
+            title='Versatile'
+            description='In a form of a tree, roadmaps can represent any kind of information hierarchical structure. They can draw logic connections between ideas and allow you to follow a sequence of steps that lead you towards your goals'
+            image='versatile'
             animate={fadeInUp}
             variants={fadeInUpAnim}
-          >
-            <div className='bg-white border border-primary rounded-full justify-center mx-auto -translate-y-16 items-center flex w-20 h-20'>
-              <img
-                src={versatile}
-                alt='versatile'
-                className='w-14 justify-center items-center'
-              />
-            </div>
-            <h1 className='text-3xl font-roboto-text font-semibold bg-gradient-to-b bg-clip-text text-transparent from-[#1A1B50] to-[#3134A5] text-center justify-center -translate-y-8'>
-              Versatile
-            </h1>
-            <p className='text-secondary text-base justify-center text-center font-medium font-roboto-text -translate-y-2'>
-              In a form of a tree, roadmaps can represent any kind of
-              information hierarchical structure. They can draw logic
-              connections between ideas and allow you to follow a sequence of
-              steps that lead you towards your goals
-            </p>
-          </motion.div>
-          <motion.div
-            className='border-primary border-t-4 w-72 h-80 2xl:w-96 py-3 px-6 bg-white drop-shadow-md items-center'
-            initial='hidden'
+          />
+          <Cards
+            title='Infinite'
+            description='Our roadmaps can be infinite! Yeah, that is right, we implemented minecraft-like chunking directly into the roadmaps, allowing for the creation and visualization of infinite amounts of data.'
+            image='infinite'
             animate={fadeInUp}
             variants={fadeInUpAnim}
-          >
-            <div className='bg-white border border-primary rounded-full justify-center mx-auto -translate-y-16 items-center flex w-20 h-20'>
-              <img
-                src={infinite}
-                alt='infinite'
-                className='w-14 justify-center items-center'
-              />
-            </div>
-            <h1 className='text-3xl font-roboto-text font-semibold bg-gradient-to-b bg-clip-text text-transparent from-[#1A1B50] to-[#3134A5] text-center justify-center -translate-y-8'>
-              Infinite
-            </h1>
-            <p className='text-secondary text-base justify-center text-center font-medium font-roboto-text -translate-y-2'>
-              Our roadmaps can be infinite! Yeah, that is right, we implemented
-              minecraft-like chunking directly into the roadmaps, allowing for
-              the creation and visualization of infinite amounts of data.
-            </p>
-          </motion.div>
-          <motion.div
-            className='border-primary border-t-4 w-72 h-80 2xl:w-96 py-3 px-6 bg-white drop-shadow-md items-center'
-            initial='hidden'
+          />
+          <Cards
+            title='Intuitive'
+            description='Roadmaps are interactive, allowing you to add comments, links, images, videos, and more. You can also share your roadmaps with others and collaborate on them in real-time.'
+            image='intuitive'
             animate={fadeInUp}
             variants={fadeInUpAnim}
-          >
-            <div className='bg-white border border-primary rounded-full justify-center mx-auto -translate-y-16 items-center flex w-20 h-20'>
-              <img
-                src={intuitive}
-                alt='intuitive'
-                className='w-14 justify-center items-center'
-              />
-            </div>
-            <h1 className='text-3xl font-roboto-text font-semibold bg-gradient-to-b bg-clip-text text-transparent from-[#1A1B50] to-[#3134A5] text-center justify-center -translate-y-8'>
-              Intuitive
-            </h1>
-            <p className='text-secondary text-base justify-center text-center font-medium font-roboto-text -translate-y-2'>
-              Roadmaps are interactive, allowing you to add comments, links,
-              images, videos, and more. You can also share your roadmaps with
-              others and collaborate on them in real-time.
-            </p>
-          </motion.div>
+          />
         </div>
       </div>
     </motion.div>
