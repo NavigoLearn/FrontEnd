ab608edc4f468aae9784843da6bee6f7d6faaa4d
erupturatis <barbulescu.eugeno@gmail.com>
2023-09-19 11:01:18 +0300

    (working)[added backlog for roadmap version]

--- src/store/roadmap-refactor/roadmap-data/params/base-roadmap.ts
+++ src/store/roadmap-refactor/roadmap-data/params/base-roadmap.ts
@@ -11,3 +11,21 @@ export const emptyRoadmap: IRoadmap = {
   },
   templates: {},
 };
+
+type IBacklog = {
+  version: string;
+  changes: string;
+  date: string;
+  status: string;
+};
+
+const ROADMAP_VERSIONS_BACKLOG: IBacklog[] = [
+  {
+    version: '1.0.1',
+    changes: 'Added fill-opacity to the roadmap nodes',
+    date: '18 sept 2023',
+    status: 'patched in nodeRenderer with a default value',
+  },
+];
+
+const ROADMAP_CURRENT_VERSION = '1.0.1';
