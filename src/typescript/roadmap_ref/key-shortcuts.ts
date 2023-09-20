25c733ddf28cbebbff608c480e6e7123b05ce6b1
sopy <contact@sopy.one>
2023-09-20 12:54:42 +0300

    Prevent default for key events
    Added `event.preventDefault()` to the keyboard shortcut event listeners in `key-shortcuts.ts`. This is to stop the default actions associated with these keys from being performed when they are used for shortcuts. It ensures that the shortcuts work as expected without triggering any unintended behavior.

--- src/typescript/roadmap_ref/key-shortcuts.ts
+++ src/typescript/roadmap_ref/key-shortcuts.ts
@@ -5,28 +5,34 @@ import {
 } from '@store/roadmap-refactor/misc/key-press-store';
 
 export function addKeyListeners() {
-  document.addEventListener('keydown', function (event) {
+  document.addEventListener('keydown', (event) => {
     if (event.shiftKey) {
+      event.preventDefault();
       setShift(true);
     }
     if (event.altKey) {
+      event.preventDefault();
       setAlt(true);
     }
     // ctrl z
     if (event.ctrlKey && event.key === 'z') {
+      event.preventDefault();
       setKey('ctrl+z', true);
     }
   });
 
-  document.addEventListener('keyup', function (event) {
+  document.addEventListener('keyup', (event) => {
     if (event.key === 'Shift') {
+      event.preventDefault();
       setShift(false);
     }
     if (event.key === 'Alt') {
+      event.preventDefault();
       setAlt(false);
     }
     // ctrl z
     if (event.key === 'z') {
+      event.preventDefault();
       setKey('ctrl+z', false);
     }
   });
