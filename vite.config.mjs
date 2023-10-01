ca2c41ab1dbf0e0fe2c39fd91dec275cf4801c3f
sopy <contact@sopy.one>
2023-10-01 22:05:47 +0300

    fix env in prod

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,5 @@
 
 import { defineConfig } from 'vite';
-import dotenv from 'dotenv';
-
-dotenv.config({ path: 'segment-env/.env' });
 
 export default defineConfig({
     // Your Vite specific config here
