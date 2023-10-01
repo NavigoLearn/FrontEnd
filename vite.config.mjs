a24409558af83e5ed4a7644037977fbf92d83b5a
erupturatis <barbulescu.eugeno@gmail.com>
2023-10-01 20:37:33 +0300

    (fix)[analytics on builditme]

---
+++ vite.config.mjs
@@ -0,0 +1,9 @@
+
+import { defineConfig } from 'vite';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: 'segment-env/.env' });
+
+export default defineConfig({
+    // Your Vite specific config here
+});
